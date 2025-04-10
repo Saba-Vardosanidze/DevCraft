ae5f22e967b0ccc238671eeb0195ce3a32c2fbd3
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-04-10 17:44:27 +0400

    feature/burgerMenu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
+import Hamburger from "hamburger-react";
+
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <>
       <header className={styles.headerContainer}>
@@ -27,11 +34,40 @@ export default function Header() {
             </li>
           </ul>
         </nav>
-        <button className={styles.burgerMenu}>
-          <img src="images/svg/burgerMenusvg.svg" alt="burgerMenu" />
+        <button className={styles.burgerMenu} onClick={toggleMenu}>
+          <Hamburger color="white" size={25} />
         </button>
         <button className={styles.headerButton}>Contact Me</button>
       </header>
+      <div className={`${styles.mobileMenu} ${isMenuOpen ? styles.show : ""}`}>
+        <ul>
+          <li className={styles.listComponent}>
+            <Link to="/" onClick={toggleMenu}>
+              Home
+            </Link>
+          </li>
+          <li className={styles.listComponent}>
+            <Link to="/about" onClick={toggleMenu}>
+              About Me
+            </Link>
+          </li>
+          <li className={styles.listComponent}>
+            <a href="#" onClick={toggleMenu}>
+              Services
+            </a>
+          </li>
+          <li className={styles.listComponent}>
+            <a href="#" onClick={toggleMenu}>
+              Portfolio
+            </a>
+          </li>
+          <li className={styles.listButton}>
+            <button className={styles.mobileContactBtn} onClick={toggleMenu}>
+              Contact Me
+            </button>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
